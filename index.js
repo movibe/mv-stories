2fbe543da2cdc00e71905e9224587d68334e71cc
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-02-11 12:28:57 -0200

    Pretty code and style in root component

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import {
-  Animated,
-  View,
-  Platform,
-  UIManager, 
-  Dimensions,
-  LayoutAnimation,
-  StatusBar,
-  PanResponder,
-  Text,
+    Animated,
+    Dimensions,
+    LayoutAnimation,
+    PanResponder,
+    Platform,
+    StatusBar,
+    UIManager,
+    View,
 } from 'react-native'
 
 import styles from './styles'
@@ -17,278 +16,276 @@ import { Bubbles, Story } from './src/components'
 const { width, height } = Dimensions.get('window')
 
 export default class Stories extends React.Component {
-  state = {
-    carouselOpen: false,
-    deckIdx: 0,
-    offset: { top: height/2, left: width/2 },
-    horizontalSwipe: new Animated.Value(0),
-    indicatorAnim: new Animated.Value(0),
-    paused: false,
-    currentStory: null,
-    swipedHorizontally: true,
-    horizontalSwipe: new Animated.Value(0),
-	  verticalSwipe: new Animated.Value(0),
-	  backOpacity: 0,
-    deckIdx: 0,
-  }
+    state = {
+        carouselOpen: false,
+        offset: { top: height / 2, left: width / 2 },
+        indicatorAnim: new Animated.Value(0),
+        paused: false,
+        currentStory: null,
+        swipedHorizontally: true,
+        horizontalSwipe: new Animated.Value(0),
+        verticalSwipe: new Animated.Value(0),
+        backOpacity: 0,
+        deckIdx: 0,
+    }
 
-  componentWillMount() {
-		if (Platform.OS == 'android')
-			UIManager.setLayoutAnimationEnabledExperimental(true)
-		this.initPanResponder()
-	}
+    componentWillMount() {
+        if (Platform.OS == 'android')
+            UIManager.setLayoutAnimationEnabledExperimental(true)
+        this.initPanResponder()
+    }
 
-  handlePress = async (idx, offset) => {
-    await this.setState({ offset, deckIdx: idx })
-    this.state.horizontalSwipe.setValue(idx * width)
+    handlePress = async (idx, offset) => {
+        await this.setState({ offset, deckIdx: idx })
+        this.state.horizontalSwipe.setValue(idx * width)
 
-		requestAnimationFrame(async () => {
-			LayoutAnimation.easeInEaseOut()
-      await this.setState({ carouselOpen: true })
-      StatusBar.setHidden(true)
-			this.animateIndicator()
-		})
-  }
+        requestAnimationFrame(async () => {
+            LayoutAnimation.easeInEaseOut()
+            await this.setState({ carouselOpen: true })
+            StatusBar.setHidden(true)
+            this.animateIndicator()
+        })
+    }
 
-  initPanResponder = () => {
-    this.panResponder = PanResponder.create({
-			onMoveShouldSetResponderCapture: () => true,
-			onMoveShouldSetPanResponderCapture: (evt, { dx, dy }) => {
-				if (Math.abs(dx) > 5) {
-					this.setState({ swipedHorizontally: true })
-					return true
-				}
+    initPanResponder = () => {
+        this.panResponder = PanResponder.create({
+            onMoveShouldSetResponderCapture: () => true,
+            onMoveShouldSetPanResponderCapture: (evt, { dx, dy }) => {
+                if (Math.abs(dx) > 5) {
+                    this.setState({ swipedHorizontally: true })
+                    return true
+                }
 
-				if (dy > 5) {
-					this.setState({ swipedHorizontally: false })
-					return true
-				}
+                if (dy > 5) {
+                    this.setState({ swipedHorizontally: false })
+                    return true
+                }
 
-				return false
-			},
+                return false
+            },
 
-			onPanResponderGrant: () => {
-				if (this.state.swipedHorizontally) {
-					this.state.horizontalSwipe.setOffset(this.state.horizontalSwipe._value)
-					this.state.horizontalSwipe.setValue(0)
-				}
+            onPanResponderGrant: () => {
+                if (this.state.swipedHorizontally) {
+                    this.state.horizontalSwipe.setOffset(this.state.horizontalSwipe._value)
+                    this.state.horizontalSwipe.setValue(0)
+                }
 
-				this.pause()
-				this.setBackOpacity(0)
-			},
+                this.pause()
+                this.setBackOpacity(0)
+            },
 
-			onPanResponderMove: (e, { dx, dy }) => {
-				if (this.state.swipedHorizontally) {
-					this.state.horizontalSwipe.setValue(-dx)
-				} else {
-					this.state.verticalSwipe.setValue(dy)
-				}
-			},
+            onPanResponderMove: (e, { dx, dy }) => {
+                if (this.state.swipedHorizontally) {
+                    this.state.horizontalSwipe.setValue(-dx)
+                } else {
+                    this.state.verticalSwipe.setValue(dy)
+                }
+            },
 
-			onPanResponderRelease: (e, { dx, dy }) => {
-				if (!this.state.swipedHorizontally) {
-					if (dy > VERTICAL_THRESHOLD) return this.leaveStories()
-					this.play()
-					return this.resetVerticalSwipe()
-				}
+            onPanResponderRelease: (e, { dx, dy }) => {
+                if (!this.state.swipedHorizontally) {
+                    if (dy > VERTICAL_THRESHOLD) return this.leaveStories()
+                    this.play()
+                    return this.resetVerticalSwipe()
+                }
 
-				this.state.horizontalSwipe.flattenOffset()
-				const deckIdx = this.state.deckIdx
+                this.state.horizontalSwipe.flattenOffset()
+                const deckIdx = this.state.deckIdx
 
-				if (dx > HORIZONTAL_THRESHOLD) { // previous deck
-					if (deckIdx == 0)
-						return this.leaveStories()
-					
-					return this.animateDeck(width * (deckIdx - 1), true)
-				}
-				
-				if (dx < -HORIZONTAL_THRESHOLD) { // -> next deck
-					if (deckIdx == this.stories.length - 1)
-						return this.leaveStories()
+                if (dx > HORIZONTAL_THRESHOLD) { // previous deck
+                    if (deckIdx == 0)
+                        return this.leaveStories()
 
-					return this.animateDeck(width * (deckIdx + 1), true)
-				}
+                    return this.animateDeck(width * (deckIdx - 1), true)
+                }
 
-				this.play()
-				return this.animateDeck(width * deckIdx)
-			}
-		})
-  }
+                if (dx < -HORIZONTAL_THRESHOLD) { // -> next deck
+                    if (deckIdx == this.stories.length - 1)
+                        return this.leaveStories()
 
-  setBackOpacity = (backOpacity) => {
-		this.state.backOpacity = backOpacity
-	}
+                    return this.animateDeck(width * (deckIdx + 1), true)
+                }
 
-  animateIndicator = (reset=true) => {
-    if (reset) this.setIndicatorAnim(0)
-		requestAnimationFrame(() => {
-			Animated.timing(this.state.indicatorAnim, {
-				toValue: 1,
-				duration: 5000 * (1-this.state.indicatorAnim._value),
-			}).start(({ finished }) => {
-				if (finished) this.onNextItem()
-			})
-		})
-  }
+                this.play()
+                return this.animateDeck(width * deckIdx)
+            },
+        })
+    }
 
-  setIndicatorAnim = (value) => {
-    this.state.indicatorAnim.setValue(value)
-  }
+    setBackOpacity = (backOpacity) => {
+        this.state.backOpacity = backOpacity
+    }
 
-  getCurrentStory = async () => {
-		if (this.props.stories.length <= 0) return null
-    await this.setState({ currentStory: this.props.stories[this.state.deckIdx] })
-    return this.state.currentStory
-	}
+    animateIndicator = (reset = true) => {
+        if (reset) this.setIndicatorAnim(0)
+        requestAnimationFrame(() => {
+            Animated.timing(this.state.indicatorAnim, {
+                toValue: 1,
+                duration: 5000 * (1 - this.state.indicatorAnim._value),
+            }).start(({ finished }) => {
+                if (finished) this.onNextItem()
+            })
+        })
+    }
 
-  onNextItem = async () => {
-    if (this.state.paused) return this.play()
+    setIndicatorAnim = (value) => {
+        this.state.indicatorAnim.setValue(value)
+    }
 
-		const story =  await this.getCurrentStory()
-		console.log('story --------', story)
+    getCurrentStory = async () => {
+        if (this.props.stories.length <= 0) return null
+        await this.setState({ currentStory: this.props.stories[this.state.deckIdx] })
+        return this.state.currentStory
+    }
 
-		if (story.idx >= story.items.length - 1)
-			return this.onNextDeck()
+    onNextItem = async () => {
+        if (this.state.paused) return this.play()
 
-		this.animateIndicator()
-		this.setStoryIdx(story.idx + 1)
-  }
+        const story = await this.getCurrentStory()
+        console.log('story --------', story)
 
-  onPrevItem = async () => {
-		if (this.backOpacity == 1) this.setBackOpacity(0);
+        if (story.idx >= story.items.length - 1)
+            return this.onNextDeck()
 
-		const story = await this.getCurrentStory()
+        this.animateIndicator()
+        this.setStoryIdx(story.idx + 1)
+    }
 
-		if (story.idx == 0)
-			return this.onPrevDeck()
+    onPrevItem = async () => {
+        if (this.backOpacity == 1) this.setBackOpacity(0)
 
-		this.animateIndicator();
-		this.setStoryIdx(story.idx - 1)
-	}
+        const story = await this.getCurrentStory()
 
-  onNextDeck = () => {
-		if (this.state.deckIdx >= this.props.stories.length - 1) return this.leaveStories()
-		this.animateDeck((this.state.deckIdx + 1)*width, true)
-	}
+        if (story.idx == 0)
+            return this.onPrevDeck()
 
-  onPrevDeck = () => {
-		if (this.state.deckIdx == 0) return this.leaveStories();
-		this.animateDeck((this.state.deckIdx - 1)*width, true);
-	}
+        this.animateIndicator()
+        this.setStoryIdx(story.idx - 1)
+    }
 
-  leaveStories = () => {
-		if (this.state.swipedHorizontally) {
-			this.animateDeck((width * this.state.deckIdx))
-		} else {
-			this.resetVerticalSwipe()
-		}
-		this.dismissCarousel()
-	}
+    onNextDeck = () => {
+        if (this.state.deckIdx >= this.props.stories.length - 1) return this.leaveStories()
+        this.animateDeck((this.state.deckIdx + 1) * width, true)
+    }
 
-  animateDeck = (toValue, reset=false) => {
-		if (reset) {
-			this.setState({ deckIdx: parseInt(toValue / width) })
-			this.animateIndicator()
-		}
-    
-		Animated.spring(this.state.horizontalSwipe, {
-			toValue, friction: 9
-		}).start()
-	}
+    onPrevDeck = () => {
+        if (this.state.deckIdx == 0) return this.leaveStories()
+        this.animateDeck((this.state.deckIdx - 1) * width, true)
+    }
 
-  resetVerticalSwipe = () => {
-		Animated.spring(this.state.verticalSwipe, { toValue: 0 }).start()
-	}
+    leaveStories = () => {
+        if (this.state.swipedHorizontally) {
+            this.animateDeck((width * this.state.deckIdx))
+        } else {
+            this.resetVerticalSwipe()
+        }
+        this.dismissCarousel()
+    }
 
-  dismissCarousel = () => {
-		LayoutAnimation.easeInEaseOut()
-		this.setState({ carouselOpen: false })
-    StatusBar.setHidden(false)
-	}
+    animateDeck = (toValue, reset = false) => {
+        if (reset) {
+            this.setState({ deckIdx: parseInt(toValue / width) })
+            this.animateIndicator()
+        }
 
-  pause = () => {
-		this.setState({ paused: true })
-		this.state.indicatorAnim.stopAnimation()
-	}
+        Animated.spring(this.state.horizontalSwipe, {
+            toValue, friction: 9,
+        }).start()
+    }
 
-	play = () => {
-		if (this.state.paused) {
-			this.setState({ paused: false })
-			this.animateIndicator(false)
-		}
-	}
+    resetVerticalSwipe = () => {
+        Animated.spring(this.state.verticalSwipe, { toValue: 0 }).start()
+    }
 
-  setStoryIdx = (idx) => {
-    const currentStory = this.state.currentStory
-    currentStory.idx = idx
-    this.setState({ currentStory })
-	}
+    dismissCarousel = () => {
+        LayoutAnimation.easeInEaseOut()
+        this.setState({ carouselOpen: false })
+        StatusBar.setHidden(false)
+    }
 
-  render() {
-    const { offset, carouselOpen } = this.state
-    const { stories } = this.props
+    pause = () => {
+        this.setState({ paused: true })
+        this.state.indicatorAnim.stopAnimation()
+    }
 
-    return (
-      <View>
-        <Bubbles stories={stories} onPress={this.handlePress} />
+    play = () => {
+        if (this.state.paused) {
+            this.setState({ paused: false })
+            this.animateIndicator(false)
+        }
+    }
 
-        <View style={[
-					styles.carouselWrap,
-					offset,
-					(carouselOpen ? styles.open : styles.closed)
-				]}>
-					<View style={styles.container} >
-            {stories.map((story, idx) => {
-              let scale = this.state.verticalSwipe.interpolate({
-                inputRange: [-1, 0, height],
-                outputRange: [1, 1, 0.75]
-              })
-              
-              if (this.state.swipedHorizontally) {
-                scale = this.state.horizontalSwipe.interpolate({
-                  inputRange: [width*(idx-1), width*idx, width*(idx+1)],
-                  outputRange: [0.79, 1, 0.78]
-                })
-              }
+    setStoryIdx = (idx) => {
+        const currentStory = this.state.currentStory
+        currentStory.idx = idx
+        this.setState({ currentStory })
+    }
 
-              return (
-                <Animated.View
-                  key={idx}
-                  style={[styles.deck, {
-                    transform: [
-                      {
-                        translateX: this.state.horizontalSwipe.interpolate({
-                          inputRange: [width*(idx-1), width*idx, width*(idx+1)],
-                          outputRange: [width, 0, -width]
-                        })
-                      },
-                      {
-                        translateY: this.state.verticalSwipe.interpolate({
-                          inputRange: [-1, 0, height],
-                          outputRange: [0, 0, height/2]
-                        })
-                      },
-                      { scale }
-                    ]
-                  }]
-                }>
-                  <Story 
-										story={story} 
-										currentDeck={this.state.deckIdx == idx}
-										onNextItem={this.onNextItem}
-										onPrevItem={this.onPrevItem}
-										pause={this.pause}
-										dismissCarousel={this.dismissCarousel}
-										setBackOpacity={this.setBackOpacity}
-										backOpacity={this.state.backOpacity}
-										indicatorAnim={this.state.indicatorAnim}
-									/>
-                </Animated.View>
-              )
-            })}
-          </View>
-				</View>
-      </View>
-    )
-  }
+    render() {
+        const { offset, carouselOpen } = this.state
+        const { stories, style } = this.props
+
+        return (
+            <View style={ style }>
+                <Bubbles stories={ stories } onPress={ this.handlePress }/>
+
+                <View style={ [
+                    styles.carouselWrap,
+                    offset,
+                    (carouselOpen ? styles.open : styles.closed),
+                ] }>
+                    <View style={ styles.container }>
+                        { stories.map((story, idx) => {
+                            let scale = this.state.verticalSwipe.interpolate({
+                                inputRange: [-1, 0, height],
+                                outputRange: [1, 1, 0.75],
+                            })
+
+                            if (this.state.swipedHorizontally) {
+                                scale = this.state.horizontalSwipe.interpolate({
+                                    inputRange: [width * (idx - 1), width * idx, width * (idx + 1)],
+                                    outputRange: [0.79, 1, 0.78],
+                                })
+                            }
+
+                            return (
+                                <Animated.View
+                                    key={ idx }
+                                    style={ [styles.deck, {
+                                        transform: [
+                                            {
+                                                translateX: this.state.horizontalSwipe.interpolate({
+                                                    inputRange: [width * (idx - 1), width * idx, width * (idx + 1)],
+                                                    outputRange: [width, 0, -width],
+                                                }),
+                                            },
+                                            {
+                                                translateY: this.state.verticalSwipe.interpolate({
+                                                    inputRange: [-1, 0, height],
+                                                    outputRange: [0, 0, height / 2],
+                                                }),
+                                            },
+                                            { scale },
+                                        ],
+                                    }]
+                                    }>
+                                    <Story
+                                        story={ story }
+                                        currentDeck={ this.state.deckIdx == idx }
+                                        onNextItem={ this.onNextItem }
+                                        onPrevItem={ this.onPrevItem }
+                                        pause={ this.pause }
+                                        dismissCarousel={ this.dismissCarousel }
+                                        setBackOpacity={ this.setBackOpacity }
+                                        backOpacity={ this.state.backOpacity }
+                                        indicatorAnim={ this.state.indicatorAnim }
+                                    />
+                                </Animated.View>
+                            )
+                        }) }
+                    </View>
+                </View>
+            </View>
+        )
+    }
 }
--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,68 +1,73 @@
 import React from 'react'
-import { View, Dimensions, TouchableWithoutFeedback, Text, SafeAreaView, ImageBackground, TouchableOpacity } from 'react-native'
+import {
+    Dimensions,
+    ImageBackground,
+    SafeAreaView,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View,
+} from 'react-native'
 import { Indicator } from '../'
-import Image from 'react-native-image-progress'
-// import CircleSnail from 'react-native-progress/CircleSnail'
-
 import styles from './styles'
+// import CircleSnail from 'react-native-progress/CircleSnail'
 
 // const circleSnailProps = { thickness: 1, color: '#ddd', size: 80 }
 const { width, height } = Dimensions.get('window')
 
 export default class Story extends React.Component {
-	render() {
-		const { story, onNextItem, pause, onPrevItem } = this.props
-		
-		return (
-			<ImageBackground source={{ uri: story.items[story.idx].src }} style={styles.deck}>
-				<TouchableOpacity
-					onPress={onPrevItem}
-					delayPressIn={200}
-					onPressIn={pause}
-					style={styles.storyAction}
-				/>
-				<TouchableOpacity
-					onPress={onNextItem}
-					delayPressIn={200}
-					onPressIn={pause}
-					style={styles.storyAction}
-				/>
-				{this.renderIndicators()}
-			</ImageBackground>
-		)
-	}
+    render() {
+        const { story, onNextItem, pause, onPrevItem } = this.props
 
-	renderCloseButton() {
-		return (
-			<TouchableWithoutFeedback onPress={this.props.dismissCarousel}>
-				<View style={styles.closeButton}>
-					<View style={[styles.closeCross, { transform: [{rotate: '45deg'}]}]} />
-					<View style={[styles.closeCross, { transform: [{rotate: '-45deg'}]}]} />
-				</View>
-			</TouchableWithoutFeedback>
-		)
-	}
+        return (
+            <ImageBackground source={ { uri: story.items[story.idx].src } } style={ styles.deck }>
+                <TouchableOpacity
+                    onPress={ onPrevItem }
+                    delayPressIn={ 200 }
+                    onPressIn={ pause }
+                    style={ styles.storyAction }
+                />
+                <TouchableOpacity
+                    onPress={ onNextItem }
+                    delayPressIn={ 200 }
+                    onPressIn={ pause }
+                    style={ styles.storyAction }
+                />
+                { this.renderIndicators() }
+            </ImageBackground>
+        )
+    }
 
-	renderIndicators() {
-		const { story, currentDeck, indicatorAnim } = this.props
+    renderCloseButton() {
+        return (
+            <TouchableWithoutFeedback onPress={ this.props.dismissCarousel }>
+                <View style={ styles.closeButton }>
+                    <View style={ [styles.closeCross, { transform: [{ rotate: '45deg' }] }] }/>
+                    <View style={ [styles.closeCross, { transform: [{ rotate: '-45deg' }] }] }/>
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
 
-		return (
-			<SafeAreaView style={styles.indicatorWrap}>
-				<View style={styles.indicators}>
-					{story.items.map((item, i) => (
-						<Indicator
-							key={i}
-							i={i}
-							animate={currentDeck && story.idx == i}
-							story={story}
-							indicatorAnim={indicatorAnim}
-						/>
-					))}
-				</View>
-				{this.renderCloseButton()}
-			</SafeAreaView>
-		)
-	}
+    renderIndicators() {
+        const { story, currentDeck, indicatorAnim } = this.props
+
+        return (
+            <SafeAreaView style={ styles.indicatorWrap }>
+                <View style={ styles.indicators }>
+                    { story.items.map((item, i) => (
+                        <Indicator
+                            key={ i }
+                            i={ i }
+                            animate={ currentDeck && story.idx == i }
+                            story={ story }
+                            indicatorAnim={ indicatorAnim }
+                        />
+                    )) }
+                </View>
+                { this.renderCloseButton() }
+            </SafeAreaView>
+        )
+    }
 }
 
 // <Components.LinearGradient
